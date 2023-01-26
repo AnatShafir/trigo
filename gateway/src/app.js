4e725603c81a43a4f2a18dac9713d0ee93438b66
anats <shafiranati@gmail.com>
2023-01-26 17:59:21 +0200

    Add log level to gateway

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -1,6 +1,12 @@
-const app = require('fastify')({ logger: true });
+const fastify = require('fastify');
 
+const { logLevel } = require('./config');
 const router = require('./router');
+
+const loggerOptions = {
+  level: logLevel,
+};
+const app = fastify({ logger: loggerOptions });
 
 app.register(router, { prefix: '/' });
 
--- gateway/src/config.js
+++ gateway/src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  logLevel: process.env.LOG_LEVEL || 'info',
   port: 80,
   msOptions: {
     servers: 'localhost:4442',
